d948bdae743cfb869cf75ac2230843b3895df3de
Nguyễn Duy Khánh <khanh@MacBook-Air-cua-Nguyen.local>
2023-03-08 15:02:25 +0700

    add .

---
+++ a.js
@@ -0,0 +1,28 @@
+let i = 10
+let f = 20.5
+let b = true
+let s = 'Hà Nội'
+let height= 10
+let width =20
+let area = width* height
+
+a = prompt('nhap a')
+b = prompt('nhap b')
+    if(a%b ==0){
+        alert('a chia het cho b')
+    }
+    else{
+        alert('a khong chia het cho b')
+    }
+
+document.write('i = '  + i)
+document.write('<br/>')
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
+document.write('s = ' + s)
+document.write('<br/>')
+document.write('dien tich = ' + area)
+
+
